4939e780096107c3bc884091ef8359e6460e2e6c
Sampo Siltanen <sampo.siltanen@gmail.com>
2024-02-16 21:02:57 +0200

    Refactored code to be more domain focused than db/api

--- Database.fs
+++ Database.fs
@@ -5,8 +5,6 @@ open System.Data
 open Microsoft.Data.Sqlite
 open Dapper.FSharp.SQLite
 
-open Utility
-
 let schema =
     """
 CREATE TABLE IF NOT EXISTS AccessToken (
@@ -23,10 +21,20 @@ CREATE TABLE IF NOT EXISTS Statistics (
 );
 """
 
-// Map nulls to options
-OptionTypes.register ()
+type AccessToken = { token: string }
 
-let connection () =
+type Statistics =
+    { id: uint
+      subject_id: uint
+      subject_type: string
+      created_at: DateTime
+      updated_at: DateTime
+      data: string }
+
+// Map database nulls with Options
+Dapper.FSharp.SQLite.OptionTypes.register ()
+
+let connection =
     async {
         let conn = new SqliteConnection("Data Source=turtles.db")
         do! conn.OpenAsync() |> Async.AwaitTask
@@ -38,67 +46,41 @@ let createIfNotExist (conn: SqliteConnection) =
     cmd.CommandText <- schema
     cmd.ExecuteNonQueryAsync() |> Async.AwaitTask |> Async.Ignore
 
-module DB =
+let insertOrReplace (conn: IDbConnection) (insertValue: 'a) =
+    insert {
+        into table<'a>
+        value insertValue
+    }
+    |> conn.InsertOrReplaceAsync
+    |> Async.AwaitTask
+    |> Async.Ignore
 
-    let insertOrReplace (conn: IDbConnection) (insertValue: 'a) =
+let insertOrReplaceMultiple (conn: IDbConnection) (insertValues: 'a[]) =
+    if Array.isEmpty insertValues then
+        async.Return()
+    else
         insert {
             into table<'a>
-            value insertValue
+            values (List.ofArray insertValues)
         }
         |> conn.InsertOrReplaceAsync
         |> Async.AwaitTask
         |> Async.Ignore
 
-    let insertOrReplaceMultiple (conn: IDbConnection) (insertValues: 'a[]) =
-        if Array.isEmpty insertValues then
-            async.Return()
-        else
-            insert {
-                into table<'a>
-                values (List.ofArray insertValues)
-            }
-            |> conn.InsertOrReplaceAsync
-            |> Async.AwaitTask
-            |> Async.Ignore
+let firstOrDefault<'a> (conn: IDbConnection) =
+    select {
+        for row in table<'a> do
+            skipTake 0 1
+    }
+    |> conn.SelectAsync<'a>
+    |> Async.AwaitTask
+    |> Async.map Seq.tryHead
 
-    let firstOrDefault<'a> (conn: IDbConnection) =
-        select {
-            for row in table<'a> do
-                skipTake 0 1
-        }
-        |> conn.SelectAsync<'a>
-        |> Async.AwaitTask
-        |> Async.map Seq.tryHead
-
-    let tryGetLatestUpdateTime<'table> (conn: IDbConnection) =
-        select {
-            for row in table<'table> do
-                max "updated_at" "latest_update"
-        }
-        |> conn.SelectAsync<{| latest_update: DateTime |}>
-        |> Async.AwaitTask
-        |> Async.map (Seq.tryHead >> (Option.map _.latest_update))
-
-module AccessToken =
-
-    type AccessToken = { token: string }
-
-    let tryGet = DB.firstOrDefault<AccessToken>
-
-    let save (conn: IDbConnection) (token: string) =
-        DB.insertOrReplace conn { token = token }
-
-module Review =
-
-    type Statistics =
-        { id: uint
-          subject_id: uint
-          subject_type: string
-          created_at: DateTime
-          updated_at: DateTime
-          data: string }
-
-    let saveStats (conn: IDbConnection) (stats: Statistics[]) = DB.insertOrReplaceMultiple conn stats
-
-    let tryGetLatestStatisticsUpdateTime (conn: IDbConnection) =
-        DB.tryGetLatestUpdateTime<Statistics> conn
+let tryGetLatestUpdateTime<'table> (conn: IDbConnection) =
+    select {
+        for row in table<'table> do
+            max "updated_at" "latest"
+    }
+    |> conn.SelectAsync<{| latest: DateTime |}>
+    |> Async.AwaitTask
+    |> Async.map (Seq.tryHead >> (Option.map _.latest))
--- Wanikani.fs
+++ Wanikani.fs
@@ -22,6 +22,9 @@ type ObjectType =
     | [<JsonName "study_material">] StudyMaterial
     | [<JsonName "voice_actor">] VoiceActor
 
+(*
+ * Metadata wrapper types for all content types
+ *)
 type Resource<'Data> =
     { id: uint
       object: ObjectType
@@ -46,137 +49,168 @@ type Collection<'Data> =
       data_updated_at: DateTime option
       data: 'Data[] }
 
-let internal wanikaniAPI token =
-    let baseUrl = "https://api.wanikani.com"
+(*
+ * Content types
+ *)
+type LessonPresentationOrder =
+    | Shuffled
+    | [<JsonName "ascending_level_then_subject">] AscendingLevelThenSubject
+    | [<JsonName "ascending_level_then_shuffled">] AscendingLevelThenShuffled
 
-    http {
-        config_transformHeader (fun (header: Header) ->
-            { header with
-                target.address = Some(baseUrl </> header.target.address.Value)
-                headers =
-                    header.headers
-                    |> Map.add "Cache-Control" "no-cache"
-                    |> Map.add "Wanikani-Revision" "20170710"
-                    |> Map.add "Authorization" $"Bearer {token}" })
+type ReviewPresentationOrder =
+    | Shuffled
+    | [<JsonName "lower_levels_first">] LowerLevelsFirst
 
-    }
+type SubscriptionType =
+    | Free
+    | Recurring
+    | Lifetime
 
-let rec internal fetchPages<'Data> token path since (pages: Collection<Resource<'Data>>[]) =
-    wanikaniAPI token {
-        GET path
-        IfModifiedSince(since |> Option.defaultValue DateTime.MinValue)
-    }
-    |> Request.sendAsync
-    |> Async.map (Response.expectHttpStatusCode System.Net.HttpStatusCode.OK)
-    |> Async.bind (function
-        | Ok res ->
-            async {
-                let! data = Response.deserializeJsonAsync<Collection<Resource<'Data>>> res
-                let pages' = Array.append pages [| data |]
+type User =
+    { id: string
+      username: string
+      level: uint
+      profile_url: Uri
+      started_at: DateTime
+      current_vacation_started_at: DateTime option
+      subscription:
+          {| ``type``: SubscriptionType
+             max_level_granted: uint
+             period_ends_at: DateTime
+             active: bool |}
+      preferences:
+          {| extra_study_autoplay_audio: bool
+             lessons_autoplay_audio: bool
+             lessons_batch_size: uint
+             lessons_presentation_order: LessonPresentationOrder
+             reviews_autoplay_audio: bool
+             reviews_display_srs_indicator: bool
+             reviews_presentation_order: ReviewPresentationOrder
+             default_voice_actor_id: uint |} }
 
-                match data.pages.next_url with
-                | Some nextUrl -> return! fetchPages<'Data> token nextUrl.PathAndQuery since pages'
-                | None -> return pages'
-            }
-        | Error expectation ->
-            printfn "%A" expectation
-            async.Return pages)
+type Lesson =
+    { available_at: DateTime
+      subject_ids: uint[] }
 
+type Review =
+    { available_at: DateTime
+      subject_ids: uint[] }
+
+type Summary =
+    { lessons: Lesson[]
+      next_reviews_at: DateTime option
+      reviews: Review[] }
+
+type SubjectType =
+    | Kanji
+    | Radical
+    | Vocabulary
+    | [<JsonName "kana_vocabulary">] KanaVocabulary
+
+type ReviewStatistics =
+    { created_at: DateTime
+      hidden: bool
+      meaning_correct: uint
+      meaning_current_streak: uint
+      meaning_incorrect: uint
+      meaning_max_streak: uint
+      percentage_correct: uint
+      reading_correct: uint
+      reading_current_streak: uint
+      reading_incorrect: uint
+      reading_max_streak: uint
+      subject_id: uint
+      subject_type: SubjectType }
+
+
+module API =
+
+    let request token =
+        let baseUrl = "https://api.wanikani.com"
+
+        http {
+            config_transformHeader (fun (header: Header) ->
+                { header with
+                    target.address = Some(baseUrl </> header.target.address.Value)
+                    headers =
+                        header.headers
+                        |> Map.add "Cache-Control" "no-cache"
+                        |> Map.add "Wanikani-Revision" "20170710"
+                        |> Map.add "Authorization" $"Bearer {token}" })
+
+        }
+
+    let rec paged<'Data> token path since (pages: Collection<Resource<'Data>>[]) =
+        request token {
+            GET path
+            IfModifiedSince(since |> Option.defaultValue DateTime.MinValue)
+        }
+        |> Request.sendAsync
+        |> Async.map (Response.expectHttpStatusCode System.Net.HttpStatusCode.OK)
+        |> Async.bind (function
+            | Ok res ->
+                async {
+                    let! data = Response.deserializeJsonAsync<Collection<Resource<'Data>>> res
+                    let pages' = Array.append pages [| data |]
+
+                    match data.pages.next_url with
+                    | Some nextUrl -> return! paged<'Data> token nextUrl.PathAndQuery since pages'
+                    | None -> return pages'
+                }
+            | Error expectation -> async.Return pages)
+
+open Database
+open System.Data
+open System.Text.Json
 
 module User =
 
-    type LessonPresentationOrder =
-        | Shuffled
-        | [<JsonName "ascending_level_then_subject">] AscendingLevelThenSubject
-        | [<JsonName "ascending_level_then_shuffled">] AscendingLevelThenShuffled
-
-    type ReviewPresentationOrder =
-        | Shuffled
-        | [<JsonName "lower_levels_first">] LowerLevelsFirst
-
-    type SubscriptionType =
-        | Free
-        | Recurring
-        | Lifetime
-
-    type User =
-        { id: string
-          username: string
-          level: uint
-          profile_url: Uri
-          started_at: DateTime
-          current_vacation_started_at: DateTime option
-          subscription:
-              {| ``type``: SubscriptionType
-                 max_level_granted: uint
-                 period_ends_at: DateTime
-                 active: bool |}
-          preferences:
-              {| extra_study_autoplay_audio: bool
-                 lessons_autoplay_audio: bool
-                 lessons_batch_size: uint
-                 lessons_presentation_order: LessonPresentationOrder
-                 reviews_autoplay_audio: bool
-                 reviews_display_srs_indicator: bool
-                 reviews_presentation_order: ReviewPresentationOrder
-                 default_voice_actor_id: uint |} }
-
-    let get (token: string) =
-        wanikaniAPI token { GET "/v2/user" }
+    let request (token: string) =
+        API.request token { GET "/v2/user" }
         |> Request.sendAsync
         |> Async.bind Response.deserializeJsonAsync<Object<User>>
 
 module Summary =
 
-    type Lesson =
-        { available_at: DateTime
-          subject_ids: uint[] }
-
-    type Review =
-        { available_at: DateTime
-          subject_ids: uint[] }
-
-    type Summary =
-        { lessons: Lesson[]
-          next_reviews_at: DateTime option
-          reviews: Review[] }
-
-    let get (token: string) =
-        wanikaniAPI token { GET "/v2/summary" }
+    let request (token: string) =
+        API.request token { GET "/v2/summary" }
         |> Request.sendAsync
         |> Async.bind Response.deserializeJsonAsync<Object<Summary>>
 
-module Review =
+module ReviewStatistics =
 
-    type SubjectType =
-        | Kanji
-        | Radical
-        | Vocabulary
-        | [<JsonName "kana_vocabulary">] KanaVocabulary
+    let request (token: string) (since: DateTime option) =
+        API.paged<ReviewStatistics> token "/v2/review_statistics" since [||]
 
-    type Statistics =
-        { created_at: DateTime
-          hidden: bool
-          meaning_correct: uint
-          meaning_current_streak: uint
-          meaning_incorrect: uint
-          meaning_max_streak: uint
-          percentage_correct: uint
-          reading_correct: uint
-          reading_current_streak: uint
-          reading_incorrect: uint
-          reading_max_streak: uint
-          subject_id: uint
-          subject_type: SubjectType }
 
-    let getStatistics (token: string) (since: DateTime option) (etag: string option) =
-        wanikaniAPI token {
-            GET "/v2/review_statistics"
-            IfModifiedSince(since |> Option.defaultValue DateTime.MinValue)
-            IfNoneMatch(etag |> Option.defaultValue "")
-        }
-        |> Request.sendAsync
-        |> Async.bind Response.deserializeJsonAsync<Collection<Resource<Statistics>>>
+    let internal mapToDb (stats: Collection<Resource<ReviewStatistics>>[]) : Statistics[] =
+        let serialize data =
+            JsonSerializer.Serialize(data, serializerOptions)
 
-    let getAllStatistics (token: string) (since: DateTime option) =
-        fetchPages<Statistics> token "/v2/review_statistics" since [||]
+        stats
+        |> Array.collect _.data
+        |> Array.map (fun stat ->
+            { id = stat.id
+              subject_id = stat.data.subject_id
+              subject_type = serialize stat.data.subject_type
+              created_at = stat.data.created_at
+              updated_at = stat.data_updated_at
+              data = serialize stat.data })
+
+    let save (conn: IDbConnection) = mapToDb >> insertOrReplaceMultiple conn
+
+    let getLatestUpdateTime (conn: IDbConnection) = tryGetLatestUpdateTime<Statistics> conn
+
+module AccessToken =
+
+    let get (conn: IDbConnection) =
+        firstOrDefault<AccessToken> conn
+        |> Async.bind (function
+            | Some { token = token } -> async.Return token
+            | None ->
+                async {
+                    Console.WriteLine "Input Wanikani Access Token:"
+                    let token = Console.ReadLine()
+                    do! insertOrReplace conn { token = token }
+                    return token
+                })
