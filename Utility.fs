b032bad166547c1c87f0d012af7487f364438c4c
Sampo Siltanen <sampo.siltanen@gmail.com>
2024-02-24 19:14:34 +0200

    Remove the unused Result.mapAsync

--- Utility.fs
+++ Utility.fs
@@ -5,12 +5,6 @@ module Async =
     let bind f computation = async.Bind(computation, f)
     let map f = bind (f >> async.Return)
 
-module Result =
-    let mapAsync f =
-        function
-        | Ok value -> async { return! f value }
-        | Error e -> async.Return e
-
 open System.Text.Json
 open System.Text.Json.Serialization
 
